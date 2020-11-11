5a848ff418f9a86d3773dabdf1b10bdf7c0342a8
Loh Wen He <wenheloh@hotmail.com>
2020-11-11 20:59:05 +0800

    get npm run build to be working

--- src/controller/esController.ts
+++ src/controller/esController.ts
@@ -2,10 +2,10 @@ import { Request as KoaRequest } from "koa";
 import { Put, Request, Route, SuccessResponse } from "tsoa";
 
 @Route("api")
-export class Controller {
+export class EsController {
   @Put("template")
   @SuccessResponse(204)
-  public async setTemplate(@Request() request: KoaRequest) {
+  public async setTemplate(@Request() _request: KoaRequest) {
 
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-import "elastic-apm-node/start";
-import "reflect-metadata";
-import { startServer } from "./server";
+import cors from "@koa/cors";
+import Router from "@koa/router";
+import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import { RegisterRoutes } from "../routes/routes";
+import { logger } from "./utils/logger";
 
-startServer();
+const startServer = async () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const app = new Koa();
+			app.use(bodyParser());
+			app.use(cors());
+
+			const router = new Router();
+			RegisterRoutes(router);
+			app.use(router.routes());
+
+			app.listen(3000, () => {
+				logger.info(`Server running on port 3000 🚀🚀🚀`);
+				resolve();
+			});
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
+startServer().catch((error) =>
+	logger.error("Error starting server => ", error)
+);
